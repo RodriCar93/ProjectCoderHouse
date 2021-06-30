ea48e432165b6131fa220df7d6d3150efd37744a
Rodrigo Cardenas <rodrigoc.93@outlook.com>
2021-06-29 22:21:10 -0300

    FIX Ancho de pagina

--- enviar.php
+++ enviar.php
@@ -1,7 +1,7 @@
 <?php
-$nombre = $_POST['nombre'];
-$mail = $_POST['email'];
-$mensaje = $_POST['mensaje'];
+$nombre .= $_POST['nombre'];
+$mail .= $_POST['email'];
+$mensaje .= $_POST['mensaje'];
 ​
 $header .= 'From: ' . $mail . " \r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
@@ -13,8 +13,8 @@ $mensaje .= "Su e-mail es: " . $mail . " \r\n";
 $mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
 $mensaje .= "Enviado el " . date('d/m/Y', time());
 ​
-$para = 'rodrigoc.93@outlook.com'; // El mail a donde van a llegar los mensajes
-$asunto = 'Mensaje de mi sitio web';
+$para  .= 'rodrigoc.93@outlook.com'; // El mail a donde van a llegar los mensajes
+$asunto .= 'Mensaje de mi sitio web';
 ​
 mail($para, $asunto, utf8_decode($mensaje), $header);
 ​
